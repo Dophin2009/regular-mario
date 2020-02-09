f87cede9b49b9abdf81faf266fe33687be2734c7
jeffy85 <60846941+jeffy85@users.noreply.github.com>
2020-02-09 11:05:53 -0500

    collisions final

--- src/collisions.js
+++ src/collisions.js
@@ -1,7 +1,18 @@
 import { sides } from "./side_enum";
 function collisionDir(moveColliderPast, moveColliderFuture, staticCollider){
     if (isColliding(moveColliderFuture, staticCollider)){
-            
+            if (moveColliderPast.y+moveColliderPast.height < staticCollider.y){
+                return sides.DOWN
+            }
+            else if (moveColliderPast.x+moveColliderPast.width < staticCollider.x){
+                return sides.RIGHT
+            }
+            else if (moveColliderPast.x > staticCollider.x + staticCollider.width){
+                return sides.LEFT
+            }
+            else {
+                return sides.UP
+            }
         }
     else {
         return sides.NONE;
@@ -9,10 +20,10 @@ function collisionDir(moveColliderPast, moveColliderFuture, staticCollider){
 }
 
 function isColliding(moveColliderFuture, staticCollider){
-    if (moveColliderFuture.x > staticCollider.x+width 
-        || moveColliderFuture.x+width < staticCollider.x
-        || moveColliderFuture.y > staticCollider.y + height
-        || moveColliderFuture.y + height < staticCollider.y){
+    if (moveColliderFuture.x > staticCollider.x+staticCollider.width 
+        || moveColliderFuture.x+moveColliderFuture.width < staticCollider.x
+        || moveColliderFuture.y > staticCollider.y + staticCollider.height
+        || moveColliderFuture.y + moveColliderFuture.height < staticCollider.y){
             return false;
         }
     else {
