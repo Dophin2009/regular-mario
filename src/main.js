fa6bd357d686ef747208ea345b53005ec1148ee0
Eric Zhao <21zhaoe@protonmail.com>
2020-02-09 14:24:17 -0500

    jakwljf

--- src/main.js
+++ src/main.js
@@ -14,8 +14,8 @@
 import { Bottleneck } from "bottleneck";
 import { calculatePlatforms } from "./generate";
 import { updatePlayer, setupKeyListener } from "./playerInput";
-import { entities, Entity } from "./entity"
-import { Player } from "./player"
+import { entities, Entity } from "./entity";
+import { Player } from "./player";
 
 export let platforms = [];
 export const g = -20;
@@ -61,8 +61,7 @@ function createCanvas() {
 function loop() {
   updatePlayer();
 
-  for (let i = 0; i < entities.length; i++)
-    entities[i].update();
+  for (let i = 0; i < entities.length; i++) entities[i].update();
 }
 
 export function start() {
@@ -78,8 +77,10 @@ export function start() {
 
   for (let i = 0; i < platforms.length; i++) {
     let p = platforms[i];
-    ctx.fillStyle = "rgb(200, 200, 200)";
-    ctx.fillRect(p.x, p.y + offset, p.width, p.height);
+    ctx.fillStyle = "rgba(200, 200, 200, 0.7)";
+    ctx.strokeStyle = "rgb(0, 0, 0)";
+    ctx.rect(p.x, p.y + offset, p.width, p.height);
+    ctx.stroke();
   }
 
   setInterval(loop, 10);
